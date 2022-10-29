6a2cdae666e04df0dff68653d6e8f90a4cc663fa
Nishant singh <114793917+nishantsingh1107@users.noreply.github.com>
2022-10-29 10:58:13 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,26 @@
+# Functions
+
+# A block of code that is executed when it is called
+
+# A function either does something ot it return something
+
+# data into functions as arguments
+
+def my_func(fName, lname):
+    print('My name is ' + fName + lname)
+    
+my_func("John", "Cena")
+
+def my_function(fname):
+  print(fname + " singh")
+
+my_function("rahul")
+my_function("ragahv")
+
+
+
+def my_function(fname, lname):
+	print(fname, lname)
+
+my_function(fname='nishant', lname='singh')
+my_function(lname='singh', fname='nishant')
